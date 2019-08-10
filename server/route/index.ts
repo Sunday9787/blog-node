5250e78a53bf4937a38574c6891dcb2c87d10732
Edward <809537981@qq.com>
2019-08-10 23:10:26 +0800

    替换count 方法使用推荐的 countDocuments

--- server/route/index.ts
+++ server/route/index.ts
@@ -18,7 +18,7 @@ router.get('/', async (ctx) => {
   const recentNews = await modelRecentNews.queryRecentNews();
   const tag = await modelTag.queryTag();
   const post = await modelPost.model.find().limit(pageConfig.pageSize);
-  const total = await modelPost.model.find().count();
+  const total = await modelPost.model.find().countDocuments();
 
   const page: PaginationConfig = { total, current: 1, baseURL: { toHome: true, base: '/page/$pager'}, ...pageConfig }
 
@@ -50,7 +50,7 @@ router.get('/page/:page', async (ctx) => {
   const recentNews = await modelRecentNews.queryRecentNews();
   const tag = await modelTag.queryTag();
   const post = await modelPost.model.find().limit(pageConfig.pageSize).skip(current)
-  const total = await modelPost.model.find().count();
+  const total = await modelPost.model.find().countDocuments();
 
   const page: PaginationConfig = { total, current, baseURL: { toHome: true, base: '/page/$pager' }, ...pageConfig }
 
@@ -80,7 +80,7 @@ router.get('/categor/:categor', async (ctx) => {
   const current = Number(ctx.query.page) || 1
   const categor: string = (ctx.params.categor as string).toLowerCase();
   const post = await modelPost.model.find({ categor }).limit(pageConfig.pageSize).skip(current);
-  const total = await modelPost.model.find({ categor }).count();
+  const total = await modelPost.model.find({ categor }).countDocuments();
 
   const page: PaginationConfig = { total, current, baseURL: `/categor/${categor}?page=$pager`, ...pageConfig }
 
@@ -99,7 +99,7 @@ router.get('/tags/:tag', async (ctx) => {
   const current = Number(ctx.query.page) || 1
   const tages: string = (ctx.params.tag as string).toLowerCase();
   const post = await modelPost.model.find({ tages }).limit(pageConfig.pageSize).skip(current);
-  const total = await modelPost.model.find({ tages }).count();
+  const total = await modelPost.model.find({ tages }).countDocuments();
 
   const page: PaginationConfig = { total, current, baseURL: `/tags/${tages}?page=$pager`, ...pageConfig }
 
